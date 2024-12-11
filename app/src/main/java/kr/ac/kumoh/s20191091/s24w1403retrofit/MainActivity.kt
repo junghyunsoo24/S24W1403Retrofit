745cc99bf5f94c02144cf43bc198e5c11afae98d
= <=>
2024-12-11 17:31:14 +0900

    🚧 SongList 완성

--- app/src/main/java/kr/ac/kumoh/s20191091/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20191091/s24w1403retrofit/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,6 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.s20191091.s24w1403retrofit.ui.theme.S24W1403RetrofitTheme
 
@@ -44,10 +52,39 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) {
 @Composable
 fun SongList(list: List<Song>, modifier: Modifier) {
     LazyColumn(
-        modifier
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp),
     ) {
+//        item{
+//            Text("header")
+//        }
         items(list){ song->
-            Text(song.toString())
+            SongItem(song)
         }
+
     }
+}
+
+@Composable
+fun SongItem(song: Song){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ){
+        TextTitle(song.title)
+        TextSinger(song.singer)
+    }
+}
+
+@Composable
+fun TextTitle(title: String){
+    Text(title, fontSize = 30.sp)
+}
+
+@Composable
+fun TextSinger(singer: String){
+    Text(singer, fontSize = 20.sp)
 }
