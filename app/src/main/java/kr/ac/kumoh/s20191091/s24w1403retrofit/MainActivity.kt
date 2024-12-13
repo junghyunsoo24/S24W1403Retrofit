fc433cefd5ccb3ffa3d1fd906331c2f5301bdd63
= <=>
2024-12-13 17:24:17 +0900

    💄 Card 사용

--- app/src/main/java/kr/ac/kumoh/s20191091/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20191091/s24w1403retrofit/MainActivity.kt
@@ -20,6 +20,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -62,7 +64,7 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) {
 @Composable
 fun SongList(list: List<Song>, modifier: Modifier) {
     LazyColumn(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(horizontal = 8.dp),
     ) {
@@ -77,31 +79,36 @@ fun SongList(list: List<Song>, modifier: Modifier) {
 }
 
 @Composable
-fun SongItem(song: Song){
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Min)
-            .background(Color(255, 210, 210))
-            .padding(8.dp)
+fun SongItem(song: Song) {
+    Card(
+        elevation = CardDefaults.cardElevation(8.dp)
     ) {
-        AsyncImage(
-            model = "https://picsum.photos/300/300?random=${song.singer}",
-            contentDescription = "가수 이미지 ${song.singer}",
-            contentScale = ContentScale.Crop,
+
+        Row(
             modifier = Modifier
-                .size(100.dp)
-                //.clip(CircleShape),
-                .clip(RoundedCornerShape(percent = 10)),
-        )
-        Spacer(modifier = Modifier.width(10.dp))
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+//                .background(Color(255, 210, 210))
+                .padding(8.dp)
         ) {
-            TextTitle(song.title)
-            TextSinger(song.singer)
+            AsyncImage(
+                model = "https://picsum.photos/300/300?random=${song.singer}",
+                contentDescription = "가수 이미지 ${song.singer}",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    //.clip(CircleShape),
+                    .clip(RoundedCornerShape(percent = 10)),
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                TextTitle(song.title)
+                TextSinger(song.singer)
+            }
         }
     }
 }
